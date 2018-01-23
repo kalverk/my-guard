22e7fa92ac5b407c4202f4fbd1d70dd7dd5d4ff1
kalverk <kalverk@hotmail.com>
2018-01-23 19:27:55 +0100

    do not keep streams open

--- app/src/main/java/com/myguard/util/Debugger.java
+++ app/src/main/java/com/myguard/util/Debugger.java
@@ -3,12 +3,10 @@ package com.myguard.util;
 import android.os.Environment;
 import android.util.Log;
 
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.FileWriter;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by kalver on 12/01/18.
@@ -16,28 +14,22 @@ import java.util.Map;
 
 public class Debugger {
 
-    private static final Map<String, FileOutputStream> streamWriterMap = new HashMap<>();
-
     public static void writeToOutputStream(String sensorName, Object[] data) {
+        BufferedWriter bufferedWriter = null;
         try {
-            if (!streamWriterMap.containsKey(sensorName)) {
-                File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), String.format("%s_%s.csv", new Date().getTime(), sensorName).replaceAll(" ", "_"));
-                Log.d("General", "Created file " + file.getAbsolutePath());
-                streamWriterMap.put(sensorName, new FileOutputStream(file));
-            }
-            streamWriterMap.get(sensorName).write(String.format("%s\n", valuesToString(data)).getBytes());
+            File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), String.format("%s_%s.csv", new Date().getTime(), sensorName).replaceAll(" ", "_"));
+            bufferedWriter = new BufferedWriter(new FileWriter(file, file.exists()));
+            bufferedWriter.write(String.format("%s\n", valuesToString(data)));
+            bufferedWriter.flush();
         } catch (Exception e) {
             Log.e("General", "Unable to write to output stream", e);
-        }
-    }
-
-    public static void closeStreams() {
-        for (Map.Entry<String, FileOutputStream> entry : streamWriterMap.entrySet()) {
-            try {
-                entry.getValue().close();
-                streamWriterMap.remove(entry.getKey());
-            } catch (Exception e) {
-                Log.e("General", "Unable to close file", e);
+        } finally {
+            if (bufferedWriter != null) {
+                try {
+                    bufferedWriter.close();
+                } catch (Exception e) {
+                    Log.e("General", "Unable to write to output stream", e);
+                }
             }
         }
     }
